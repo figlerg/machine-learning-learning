e84b5d10227dc39138ca916af6eb472d6fd35ede
DavidMLDTU <d.onnen@web.de>
2019-09-17 16:33:13 +0200

    data folder added
    -

--- PCA.py
+++ PCA.py
@@ -5,11 +5,10 @@ Created on Mon Sep 16 16:39:13 2019
 @author: louis
 """
 from Init import *
-
 from matplotlib.pyplot import figure, plot, title, xlabel, ylabel, show, legend
 from scipy.linalg import svd
 
-Y = X - np.ones((N,1))*X.mean(0)
+Y  = X - np.ones((N,1))*X.mean(0)
 Y1 = Y*(1/np.std(Y,0))
 
 U,S,Vh = svd(Y1,full_matrices=False)
